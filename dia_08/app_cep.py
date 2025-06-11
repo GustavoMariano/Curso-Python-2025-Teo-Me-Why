fb3de5d69c4af48a4193791d5e5b04297daa9de3
Gustavo Mariano <gustavocmariano@hotmail.com>
2025-06-11 00:38:10 -0300

    dia 08 - Chamada em API e streamlit

---
+++ dia_08/app_cep.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+import requests
+
+url = "https://viacep.com.br/ws/{cep}/json/"
+
+st.title("Busca CEP")
+
+cep = st.text_input("Busque o seu cep")
+
+if cep != "":
+    try:
+        resp = requests.get(url.format(cep=cep))
+        data = pd.DataFrame([resp.json()])
+        st.dataframe(data)
+        
+    except Exception as err:
+        st.error("Entre com um cep válido!")
